ee997facf7ccbfa791e70bee7dbe3398987f774f
misba1188 <31615034+misba1188@users.noreply.github.com>
2017-09-07 09:14:21 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+for i in range (1,21):
+    if(i%2==0):
+        print(i)
+for i in range (1,31):
+    if (i%3==0):
+        print(i)
+for i in range (1,41):
+    if (i%4==0):
+        print(i)
+for i in range (1,60):
+    if (i%5==0):
+        print(i)
