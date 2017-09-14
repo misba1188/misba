32e7f0554487eab83895ea8339ec89ddba21aea2
misba1188 <31615034+misba1188@users.noreply.github.com>
2017-09-14 10:38:34 +0530

    Add files via upload

---
+++ mateen.py
@@ -0,0 +1,10 @@
+program 1=rock,paper,scissor from www.gist.github.com
+in rock,paper scissor i have 4 modules.computer,player,input from player,result
+no changes has been done from www.gist.github.com
+program 2=snake and ladder
+by own . i have 4 module board,dice, players ,snake ,ladder result
+progam 3= email,
+stmp,to,from,password,login
+program 4=tables 
+ 
+
