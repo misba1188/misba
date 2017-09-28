88c820c899f4e36f5b576d459d996128721ffb5a
misba1188 <31615034+misba1188@users.noreply.github.com>
2017-09-28 09:32:27 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 for i in range (1,21):
+    # the value of i in the range frm 1 to 21
     if(i%2==0):
         print(i)
+        #printing i value
 for i in range (1,31):
+      
     if (i%3==0):
         print(i)
 for i in range (1,41):
