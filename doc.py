2b3db2dda916bd9a2125feceddd925b2dc044f09
misba1188 <31615034+misba1188@users.noreply.github.com>
2017-09-28 09:02:43 +0530

    Update doc.py

--- doc.py
+++ doc.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 import smtplib
+#from address
+#simple mail trans support
 s = smtplib.SMTP('smtp.gmail.com', 587)
+#mail address
 s.starttls()
 s.login("misbaanjum82255@gmail.com","mateen123")
+#sending to address with pasword
 message = "hi hello,how r u"
+#sending messages
 s.sendmail("misbaanjum82255@gmail.com","misbaanjum82255@gmail.com",message)
 s.quit()
+#sending mail using phyton program
