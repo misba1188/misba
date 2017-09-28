f26969e7472f40e4c09d02d95bcb2d5d7c83f53f
misba1188 <31615034+misba1188@users.noreply.github.com>
2017-09-28 08:53:12 +0530

    Update anjum2.py

--- anjum2.py
+++ anjum2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # guess what this prog does???
 import random 
+# this program choose random
 r=random.randint(1,6) # gives the dice value
+# r chooses the no frm 1 to 6
 print ("random number generated is =",r)
 if r==6 :
  	print ("congrates i have got 6 ")
+  # the system chose random no 6 because r=6
